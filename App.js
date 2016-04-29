65e5db1f101022b9cdfecb89070d61ecb05f33e0
Sonu <harsh.mittal@yahoo.com>
2016-04-29 06:29:27 +0000

    My first commit to git

---
+++ App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function() {
+        //Write app code here
+        console.log("sjkfhrfjfkrfjklekfjrfrfkjferklf")
+        alert("hello")
+        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+    }
+});
